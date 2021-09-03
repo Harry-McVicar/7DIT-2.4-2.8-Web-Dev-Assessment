562c913f92664472dc33e9e0c39b53c7f4d0f975
Harry-McVicar <18hmcvicar@wakatipu.school.nz>
2021-09-04 11:32:04 +1200

    I followed a tutorial in order to create a javascript quiz for the user's to test their understanding of each CS concept. So far, I have only created questions for the encryption quiz so will have to proceed to integrate a quiz for the other two CS concepts. The generate quiz function consists of a function to display the questions, which uses radio buttons for the user to select their answer, as well as a function to display the results, turning the correct answers green and the incorrect answers red as well as showing the user's score. I also added some more content to the tabs component and used another css variable for font to improve adaptability.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/*Tabs system*/
+
 const btns = document.querySelectorAll(".tab-btn");
 const contents = document.querySelectorAll(".content");
 const container = document.querySelector(".container");
@@ -16,3 +18,124 @@ container.addEventListener("click", function (e) {
     element.classList.add("active");
   }
 })
+
+/*Javascript quiz*/
+
+var encryptionQuestions = [
+  {
+    question: "1. Encryption",
+    answers: {
+      a: 'The process of tuning plaintext into scrambled ciphertext, which can only be understood if it is decrypted',
+      b: 'The process of deciphering encrypted data or messages',
+      c: 'A substitution cipher where one character of plaintext is substituted for another, which becomes the ciphertext'
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "2. Decryption",
+    answers: {
+      a: 'The process of tuning plaintext into scrambled ciphertext, which can only be understood if it is decrypted',
+      b: 'The process of deciphering encrypted data or messages',
+      c: 'A substitution cipher where one character of plaintext is substituted for another, which becomes the ciphertext'
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "3. Caesar Cipher",
+    answers: {
+      a: 'The process of tuning plaintext into scrambled ciphertext, which can only be understood if it is decrypted',
+      b: 'The process of deciphering encrypted data or messages',
+      c: 'A substitution cipher where one character of plaintext is substituted for another, which becomes the ciphertext'
+    },
+    correctAnswer: 'c'
+  },
+  {
+    question: "4. Plaintext",
+    answers: {
+      a: 'Data in human-readable form',
+      b: 'Data that has been encrypted',
+      c: 'In cryptography it is the data that is used to encrypt and decrypt the data'
+    },
+    correctAnswer: 'a'
+  },
+  {
+    question: "5. Ciphertext",
+    answers: {
+      a: 'Data in human-readable form',
+      b: 'Data that has been encrypted',
+      c: 'In cryptography it is the data that is used to encrypt and decrypt the data'
+    },
+    correctAnswer: 'b'
+  },
+  {
+    question: "6. Key",
+    answers: {
+      a: 'Data in human-readable form',
+      b: 'Data that has been encrypted',
+      c: 'In cryptography it is the data that is used to encrypt and decrypt the data'
+    },
+    correctAnswer: 'c'
+  }
+];
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
+
+  function showQuestions(questions, quizContainer) {
+    var output = [];
+
+    for(var i=0; i<questions.length; i++) {
+      answers = [];
+
+      for(letter in questions[i].answers) {
+        answers.push(
+          '<label>'
+            + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+            + letter + ': '
+            + questions[i].answers[letter]
+          + '</label>' + '\n'
+        );
+      }
+
+      output.push(
+        '<div class="question">' + questions[i].question + '</div>'
+        + '<div class="answers">' + answers.join('') + '</div>'
+      );
+    }
+
+    quizContainer.innerHTML = output.join('');
+  }
+
+
+  function showResults(questions, quizContainer, resultsContainer) {
+
+    var answerContainers = quizContainer.querySelectorAll('.answers');
+    var userAnswer = '';
+    var numCorrect = 0;
+
+    for(var i=0; i<questions.length; i++) {
+      userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+
+      if(userAnswer===questions[i].correctAnswer) {
+        numCorrect++;
+        answerContainers[i].style.color = 'lightgreen';
+      }
+      else {
+        answerContainers[i].style.color = 'red';
+      }
+    }
+
+    resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+  }
+
+  showQuestions(questions, quizContainer);
+
+  submitButton.onclick = function(){
+    showResults(questions, quizContainer, resultsContainer);
+  }
+}
+
+generateQuiz(encryptionQuestions, quizContainer, resultsContainer, submitButton);
