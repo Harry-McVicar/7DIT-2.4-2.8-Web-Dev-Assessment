80d0a5ef4276ad4f766ae172f9ccae897d0bab61
Harry-McVicar <18hmcvicar@wakatipu.school.nz>
2021-09-24 09:58:27 +1200

    I used a media query to make the navigation bar arrange itself into a column and reduce the size of the font at a screen size below 800px. This could potentially be developed further so that it forms a side bar at a mobile scale. I also removed the unnecessary colour change of the items in the drag and drop section so that they remain turquoise after being dragged. By putting the images on the details section within a div and setting the display property to flex, I was easily able to center the content.

--- app.js
+++ app.js
@@ -350,11 +350,6 @@ function onDragStart(event) {
   event
     .dataTransfer
     .setData('text/plain', event.target.id);
-
-  event
-    .currentTarget
-    .style
-    .backgroundColor = '#DFA612';
 }
 
 function onDragOver(event) {
